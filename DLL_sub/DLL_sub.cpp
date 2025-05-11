a9fe8c9ff26486789ca1c25cf025257b76b986f3
h1sql <1157578731@qq.com>
2025-05-11 22:22:58 +0800

    c++实验

---
+++ DLL_sub/DLL_sub.cpp
@@ -0,0 +1,11 @@
+﻿// DLL_sub.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "DLL_sub.h"
+
+
+DLLSUB_API int sub_fun(int a, int b) {
+	return a - b;
+}
---
+++ Dll1/Dll1.cpp
@@ -0,0 +1,14 @@
+﻿// Dll1.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Dll1.h"
+
+
+
+DLL1_API int add_fun(int a, int b) {
+	return a + b;
+}
+
+
